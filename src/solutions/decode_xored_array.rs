0ebba9a69b072b97924464b5df8079c9a231edea
egregors <root@egregors.com>
2021-10-01 13:59:51 +0300

    1720. Decode XORed Array solved

---
+++ src/solutions/decode_xored_array.rs
@@ -0,0 +1,36 @@
+/** 
+ * https://leetcode.com/problems/decode-xored-array/
+ * 
+ * There is a hidden integer array arr that consists of n non-negative integers.
+ * 
+ * It was encoded into another integer array encoded of length n - 1, such that 
+ * encoded[i] = arr[i] XOR arr[i + 1]. For example, if arr = [1,0,2,1], then encoded = [1,2,3].
+ * 
+ * You are given the encoded array. You are also given an integer first, that is the first 
+ * element of arr, i.e. arr[0].
+ * 
+ * Return the original array arr. It can be proved that the answer exists and is unique. 
+ * 
+ **/
+
+struct Solution {}
+
+impl Solution {
+    pub fn decode(encoded: Vec<i32>, first: i32) -> Vec<i32> {
+        let mut decoded = vec![first];
+        for i in 0..encoded.len() {
+            decoded.push(decoded[i] ^ encoded[i]);
+        }
+        decoded
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_decode() {
+        assert_eq!(Solution::decode(vec![1, 2, 3], 1), vec![1, 0, 2, 1]);
+        assert_eq!(Solution::decode(vec![6,2,7,3], 4), vec![4,2, 0,7,4]);
+    }
+}
--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -1,3 +1,4 @@
 mod kids_with_candies;
 mod num_identical_pairs;
 mod parking_system;
+mod decode_xored_array;
