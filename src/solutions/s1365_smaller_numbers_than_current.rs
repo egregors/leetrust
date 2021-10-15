1ec2e0302c00e9a4750f7daa193d4ceba8ca91d4
egregors <root@egregors.com>
2021-10-15 13:58:33 +0300

    cleanup

--- src/solutions/s1365_smaller_numbers_than_current.rs
+++ src/solutions/s1365_smaller_numbers_than_current.rs
@@ -9,8 +9,10 @@
  *
  **/
 
+#[allow(dead_code)]
 struct Solution {}
 
+#[allow(dead_code)]
 impl Solution {
     pub fn smaller_numbers_than_current(nums: Vec<i32>) -> Vec<i32> {
         let mut res = vec![];
